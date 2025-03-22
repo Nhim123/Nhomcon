646dc305e7a20bc27ed48c89da9d9de7bafea2de
Nhim123 <nhimhuydiet123@gmail.com>
2025-03-22 12:36:43 +0700

    Revert "22.3.2025"
    This reverts commit 0c995270558e019ec2dc8f00d5f2a16aabbc17fb.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ export const Footer = () => {
             <img src={Logosmall} alt="Logo" className="h-12 mr-3" />
         </div>
 
-        <div className='grid grid-cols-3 space-x-10'>
+        <div className='grid grid-cols-3'>
           <div className='grid grid-rows pt-5'>
             <div className='flex items-center'>
               <span className="material-symbols-outlined text-white h-12 w-12 flex-shrink-0">location_on</span>
@@ -27,38 +27,8 @@ export const Footer = () => {
               <h1 className='text-wrap font-montserrat font-semibold text-white text-justify ml-5  flex-grow'>ha@hunganh.org.vn</h1>
             </div>
           </div>
-
-          <div className='grid grid-rows pt-5 text-white border-l-2 border-white pl-8'>
-            <p className='font-NunitoSans text-2xl font-bold'>Our Services</p>
-            <h1>Supply Chain Management (SCM)</h1>
-            <h2>Artificial Intelligence (AI)</h2>
-            <h3>Website Development</h3>
-            <h4>Mobile-Application Development</h4>
-          </div>
-
-          <div className='grid grid-rows pt-5 text-white border-l-2 border-white pl-8'>
-            <p className='font-NunitoSans text-2xl font-bold'>Follow Us</p>
-              <div className='flex items-center'>
-                <img src={FacebookLogo} alt="Facebook" className="h-8 w-8" />
-                <h1 className='text-wrap font-montserrat font-semibold text-white text-justify ml-5 capitalize flex-grow'>Facebook</h1>
-              </div>
-              
-              <div className='flex items-center'>
-                <img src={Whatsapp} alt="Whatsapp" className="h-8 w-8" />
-                <h1 className='text-wrap font-montserrat font-semibold text-white text-justify ml-5 capitalize flex-grow'>Whatsapp</h1>
-              </div>
-
-              <div className='flex items-center'>
-                <img src={LinkedInLogo} alt="LinkedIn" className="h-8 w-8" />
-                <h1 className='text-wrap font-montserrat font-semibold text-white text-justify ml-5 capitalize flex-grow'>LinkedIn</h1>
-              </div>
-          </div>
-        </div>
-        <div className='text-center text-white mt-5 uppercase font-semibold font-NunitoSans'>
-          <p>&copy; 2025 Hunganh Company. All rights reserved.</p>
         </div>
       </div>
-       
     </footer>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import  Logo  from "../assets/Logo.svg";
 import { AiOutlineMenu , AiOutlineClose } from "react-icons/ai";
-import Ai  from './Servicecomponent/Ai.jsx';
-import Webapp  from './Servicecomponent/Webapp.jsx'
-import Mobileapp from './Servicecomponent/Mobileapp.jsx'
 
 export const Nav = () => {
   const [nav, setNav] = useState(false)
-  const [dropdown, setDropdown] = useState(false)
 
   const handleNav = () => {
     setNav(!nav)
@@ -27,20 +23,7 @@ export const Nav = () => {
           <ul className=' flex justify-end items-center gap-16 max-lg:hidden'>
             <a href='/' className='text-xl font-semibold'>Home</a>
             <a href='/about' className='text-xl font-semibold'>About</a>
-            <div 
-              className='relative'
-              onMouseEnter={() => setDropdown(true)}
-              onMouseLeave={() => setDropdown(false)}
-            >
-              <a href='/services' className='text-xl font-semibold'>Services </a>
-              {dropdown && (
-                <ul className='absolute left-0 top-[100%] mt-2 w-screen bg-white shadow-lg'>
-                  <li><a href='/services/service1' className='block px-4 py-2'>Service 1</a></li>
-                  <li><a href='/services/service2' className='block px-4 py-2'>Service 2</a></li>
-                  <li><a href='/services/service3' className='block px-4 py-2'>Service 3</a></li>
-                </ul>
-              )}
-            </div>
+            <a href='/services'className='text-xl font-semibold'>Services</a>
             <a href='/contact' className='text-xl font-semibold'>Contact Us</a>
             <a href='/test' className='text-xl font-semibold'>Test</a>
           </ul>
@@ -51,11 +34,6 @@ export const Nav = () => {
                 <li href='/' className='p-4 border-b border-gray-600'>Home</li>
                 <li href='/about' className='p-4 border-b border-gray-600'>About</li>
                 <li href='/services'className='p-4 border-b border-gray-600'>Services</li>
-                <ul className='pl-4'>
-                  <li><a href='/services/service1' className='block py-2'>Service 1</a></li>
-                  <li><a href='/services/service2' className='block py-2'>Service 2</a></li>
-                  <li><a href='/services/service3' className='block py-2'>Service 3</a></li>
-                </ul>
                 <li href='/contact' className='p-4 border-b border-gray-600'>Contact Us</li>
                 <li href='/test'className='p-4 border-b border-gray-600'>Test</li>
               </ul>
